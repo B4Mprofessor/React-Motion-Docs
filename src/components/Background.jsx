30922e33f6a2c702dffb766897852ca3ea52a43c
abhik sahana <abconnect.me@gmail.com>
2024-01-04 14:28:58 +0530

    Init

---
+++ src/components/Background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <div className="w-full h-screen fixed z-[2]">
+        <div className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-l font-semibold">
+          Documents.
+        </div>
+        <h1 className="absolute text-[13vw] leading-none tracking-tighter -translate-x-[50%] -translate-y-[50%]  top-1/2 left-1/2 font-semibold text-zinc-900">
+          Docs.
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Background;
